34f5600afe3fa941899b3d2768003c2d65225b4f
Vedpisharody <95162067+Vedpisharody@users.noreply.github.com>
2024-01-17 19:49:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload() {
+
+}
+
+
+function setup() {
+    canvas = createCanvas(1500, 500);
+    canvas.position(110, 250);
+
+
+}
+
+function draw() {
+    fill(255, 23, 0);
+  circle =  circle(200, 200, 340, );
+  ellipse = ellipse(650, 200, 400, 250);
+  rect =  rect(1000, 150, 400, 300);
+    
+
+}
